40e836dcd913a65820075571d58d85f2c061ebb9
pgilp <pgilp17@gmail.com>
2022-08-04 15:00:42 +0200

    Fin Lightbox

--- js/functions.js
+++ js/functions.js
@@ -2,65 +2,46 @@ $(window).on("load", function(){
 
     //Calcular tamaño image-slider
     var heightHeader = $("header").outerHeight();
-    console.log("calc(100vh - "+heightHeader+"px)");
     $(".images-slider").height("calc(100vh - "+heightHeader+"px)");
     //
 })
 
 $(document).ready(function(){
+
+    
     
     //Funcion de cambio de imagen al clickar en las flechas
     $(".images-slider .arrow").click(function(){
-        var maximo = 3;
-        var imagen =  parseInt($(".new").attr("src").substring(19,20));
-        var active = !$(this).hasClass("disabled");
-        console.log(imagen);
- 
-
-        if(imagen == maximo && active ){
-            console.log("imagen = maximo");
-            if($(this).hasClass("previous")){
-                imagen -=1;
-                $(".new").attr("src", "imgs/JPG/Novedades/"+(imagen)+".jpg");
-            }
-        }else if(imagen == 1 && active){
-            if($(this).hasClass("next")){
-                imagen +=1;
-                $(".new").attr("src", "imgs/JPG/Novedades/"+(imagen)+".jpg");
-            }
-        }else if(active){
-            console.log("else");
-            if($(this).hasClass("next")){
-                imagen +=1;
-                $(".new").attr("src", "imgs/JPG/Novedades/"+(imagen)+".jpg");
-            }else if($(this).hasClass("previous")){
-                imagen -=1;
-                $(".new").attr("src", "imgs/JPG/Novedades/"+(imagen)+".jpg");
-            }
-        }
-        
-
-        if(imagen == maximo){
-            $(".images-slider .arrow.next").addClass("disabled");
-        }else if(imagen == 1){
-            $(".images-slider .arrow.previous").addClass("disabled");
-        }else{
-            $(".images-slider .arrow").removeClass("disabled");
-        }
-
+        var quantity = parseInt($(".news").data("quantity"));
+        var imagen   =  parseInt($(".new").attr("src").substring(19,20));
+        var active   = !$(this).hasClass("disabled");
+        var url      = "imgs/JPG/Novedades/";
+        arrowFeature(quantity, imagen , active, $(this),url,$(".new"), ".images-slider");
     });
     //
 
     // Lightbox
 
     //Abrir y cargar contenido
+
+    var claseFlecha    ;
+    var urlImgNoPrefix ;
+    var quantity       ;
+
     $(".product.card").click(function(){
         $(".lightbox").show();
         $("body").addClass("scrollbarInvisible");
 
-        var urlImgNoPrefix = "imgs/JPG/" + $(this).data("product") + "/";
-        
-        var quantity = parseInt($(this).data("quantity"));
+        claseFlecha    = ".lightbox";
+        urlImgNoPrefix = "imgs/JPG/" + $(this).data("product") + "/";
+        quantity       = parseInt($(this).data("quantity"));
+
+        if(quantity == 1){
+            $(claseFlecha+" .arrow.next").addClass("disabled");
+            $(claseFlecha+" .arrow.previous").addClass("disabled");
+        }else{
+            $(claseFlecha+" .arrow.next").removeClass("disabled");
+        }
 
         for (let index = 1; index <= quantity; index++) {
             
@@ -70,29 +51,135 @@ $(document).ready(function(){
                 $(".img-principal").append('<img class="principal-img-lihgtbox" src='+urlImgFinal+'>');
             }    
         
-            console.log(urlImgFinal);
-
-            $(".container-imgs-secondary").append('<img class="little-img-lihgtbox" width="20%" src='+urlImgFinal+'>');
+            $(".container-imgs-secondary").append('<img class="little-img-lihgtbox" src='+urlImgFinal+'>');
             
         }
 
-        $(".little-img-lihgtbox").click(function(){
+        $(".little-img-lihgtbox").click(function(event){
+            event.stopPropagation();
+
             var newUrlImgPrincipal = $(this).attr("src");
+            var numImagen          = parseInt(newUrlImgPrincipal.substr(newUrlImgPrincipal.length -5 ,1));
             $(".container-img-principal .img-principal").empty();
             $(".img-principal").append('<img class="principal-img-lihgtbox" src='+newUrlImgPrincipal+'>');
+
+            if(numImagen == quantity){
+                console.log("numImagen==quantity");
+                $(claseFlecha+" .arrow.next").addClass("disabled");
+                $(claseFlecha+" .arrow.previous").removeClass("disabled");
+            }else if(numImagen == 1){
+
+                console.log("numImagen==1");
+
+                $(claseFlecha+" .arrow.previous").addClass("disabled");
+                $(claseFlecha+" .arrow.next").removeClass("disabled");
+            }else{
+
+                console.log("else");
+                $(claseFlecha+" .arrow").removeClass("disabled");
+            }
+
         });
+
+
         
     });
 
+    //FUncionamiento flechas
+
+    $(".lightbox .arrow").click(function(event){
+        event.stopPropagation();
+
+        var srcImagen = $(".principal-img-lihgtbox").attr("src");
+        var numImagen = parseInt(srcImagen.substr(srcImagen.length -5 ,1));
+        var active    = !$(this).hasClass("disabled");
+        
+        arrowFeature(quantity, numImagen , active, $(this),urlImgNoPrefix,$(".principal-img-lihgtbox"),claseFlecha);
+    });
+
+
     //Cerrar y vaciar contenido
-    $(".lightbox .hide").click(function(){
+
+    $(".lightbox .hide, .lightbox").click(function(event){
+
+        $(".lightbox .arrow.previous").addClass("disabled");
+        $(".lightbox  .arrow.next").removeClass("disabled");
+
         $(".lightbox").hide();
         $("body").removeClass("scrollbarInvisible");
 
         $(".container-imgs-secondary").empty();
         $(".container-img-principal .img-principal").empty();
-
     });
+
     //
 
+    function arrowFeature(quantity, imagen, active, arrow, url, claseImagen, claseFlecha){
+
+        if(imagen == quantity && active ){
+            if(arrow.hasClass("previous")){
+                imagen -=1;
+                claseImagen.attr("src", url+(imagen)+".jpg");
+            }
+        }else if(imagen == 1 && active){
+            if(arrow.hasClass("next")){
+                imagen +=1;
+                claseImagen.attr("src", url+(imagen)+".jpg");
+            }
+        }else if(active){
+            if(arrow.hasClass("next")){
+                imagen +=1;
+                claseImagen.attr("src", url+(imagen)+".jpg");
+            }else if(arrow.hasClass("previous")){
+                imagen -=1;
+                claseImagen.attr("src", url+(imagen)+".jpg");
+            }
+        }
+        
+        if(imagen == quantity){
+            $(claseFlecha+" .arrow.next").addClass("disabled");
+            $(claseFlecha+" .arrow.previous").removeClass("disabled");
+        }else if(imagen == 1){
+            $(claseFlecha+" .arrow.previous").addClass("disabled");
+            $(claseFlecha+" .arrow.next").removeClass("disabled");
+        }else{
+            $(claseFlecha+" .arrow").removeClass("disabled");
+        }
+        
+    }
+
 });
+
+
+
+
+
+// if(imagen == quantity && active ){
+//     console.log("imagen = quantity");
+//     if($(this).hasClass("previous")){
+//         imagen -=1;
+//         $(".new").attr("src", "imgs/JPG/Novedades/"+(imagen)+".jpg");
+//     }
+// }else if(imagen == 1 && active){
+//     if($(this).hasClass("next")){
+//         imagen +=1;
+//         $(".new").attr("src", "imgs/JPG/Novedades/"+(imagen)+".jpg");
+//     }
+// }else if(active){
+//     console.log("else");
+//     if($(this).hasClass("next")){
+//         imagen +=1;
+//         $(".new").attr("src", "imgs/JPG/Novedades/"+(imagen)+".jpg");
+//     }else if($(this).hasClass("previous")){
+//         imagen -=1;
+//         $(".new").attr("src", "imgs/JPG/Novedades/"+(imagen)+".jpg");
+//     }
+// }
+
+// if(imagen == quantity){
+//     $(".images-slider .arrow.next").addClass("disabled");
+// }else if(imagen == 1){
+//     $(".images-slider .arrow.previous").addClass("disabled");
+// }else{
+//     $(".images-slider .arrow").removeClass("disabled");
+// }
